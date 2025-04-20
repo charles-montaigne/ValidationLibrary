9b49218d8b89b6ccad83a941ad616dbbef16d7d3
Charles <charles.montaigne@gmail.com>
2025-04-20 18:10:12 +0200

    JsonPointer Property

---
+++ src/ValidationLibrary/JsonPointer.cs
@@ -0,0 +1,26 @@
+﻿namespace ValidationLibrary;
+
+/// <summary>
+/// Represent a JsonPointer https://datatracker.ietf.org/doc/html/rfc6901
+/// </summary>
+public sealed class JsonPointer : IProperty
+{
+    public string Value { get; }
+
+    private JsonPointer(string value)
+    {
+        Value = value;
+    }
+
+    private static string Camel(string propertyName)
+    {
+        return System.Text.Json.JsonNamingPolicy.CamelCase.ConvertName(propertyName);
+    }
+
+    public static JsonPointer OfRootElement => new JsonPointer(rootElement);
+    private static readonly string rootElement = "#";
+
+    public IProperty WithSubProperty(string propertyName) => new JsonPointer($"{Value}/{Camel(propertyName)}");
+    //public IProperty WithSubProperty(string collectionName, int index) => new JsonPointer($"{Value}/{Camel(collectionName)}/{index}");
+    //public IProperty WithIndex(int index) => new JsonPointer($"{Value}/{index}");
+}
